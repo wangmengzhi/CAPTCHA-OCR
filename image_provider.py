f7365cea1f444ade13a40a20e3fbbbe468bb80d0
wangmengzhi <mengzhiwang@live.com>
2017-02-14 12:32:42 +0800

    Add files via upload

---
+++ image_provider.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+# 提供训练数据
+# created by wmz
+# copyright USTC
+# 2017-02-14
+
+import io
+import random
+
+import paddle.utils.image_util as image_util
+from paddle.trainer.PyDataProvider2 import *
+
+@provider(input_types=[
+    dense_vector(28 * 28),
+    integer_value(62)
+])
+def processData(settings, file_name):
+    """
+    加载数据
+    迭代每一批的所有图像和标签
+    file_name: 批文件名
+    """
+    #使用pickle类来进行python对象的序列化，而cPickle提供了一个更快速简单的接口，如python文档所说的：“cPickle -- A faster pickle”
+    data = cPickle.load(io.open(file_name, 'rb'))
+    #list()方法用于将元组转换为列表，元组与列表的区别在于元组的元素值不能修改，元组是放在括号中，列表是放于方括号中。
+    indexes = list(range(len(data['images'])))
+    random.shuffle(indexes)
+    for i in indexes:
+        #加载图像，img:(K x H x W) ndarrays
+        img = image_util.decode_jpeg(data['images'][i])
+        label = data['labels'][i]
+        '''
+	    包含yield语句的函数会被特地编译成生成器。当函数被调用时，他们返回一个生成器对象
+	    不像一般函数生成值后退出，生成器函数生成值后会自动挂起并暂停他们的执行和状态，他的本地变量将保存状态信息，这些信息在函数恢复时将再度有效
+	    执行到 yield时，processData 函数就返回一个迭代值，下次迭代时，代码从 yield的下一条语句继续执行
+	    '''
+        yield img.flatten().tolist(), int(label)
---
+++ vgg_16_captcha.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+# 网络配置文件
+# created by wmz
+# copyright USTC
+# 2017-02-14
+
+from paddle.trainer_config_helpers import *
+
+is_predict = get_config_arg("is_predict", bool, False)
+
+if not is_predict:
+    define_py_data_sources2(
+        train_list='data/batches/train.list',
+        test_list='data/batches/test.list',
+        module='image_provider',
+        obj='processData')
+
+settings(
+    batch_size = 128,
+    learning_rate = 0.1 / 128.0,
+    learning_method = MomentumOptimizer(0.9),
+    #权重衰减，防过拟合
+    regularization = L2Regularization(0.0005 * 128)
+)
+
+img = data_layer(name='image',size=1*28*28)
+predict = small_vgg(input_image=img,num_channels=1,num_classes=62)
+
+if not is_predict:
+    lbl = data_layer(name="label", size=62)
+    outputs(classification_cost(input=predict, label=lbl))
+else:
+    outputs(predict)
